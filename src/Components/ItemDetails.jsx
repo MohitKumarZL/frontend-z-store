c5da8ffe154b301466b9ed104c5bbe85217a35c7
Mohit Kumar <mohitkumarzl@hotmail.com>
2025-06-04 11:52:12 +0530

    Item details created

---
+++ src/Components/ItemDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ItemDetails = () => {
+  return (
+    <div>
+      {/* Image section */}
+      <div className='grid grid-cols-2'>
+
+      </div>
+    </div>
+  )
+}
+
+export default ItemDetails
